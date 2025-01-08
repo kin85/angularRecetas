355028626eaab10c211b7c1aa80c832e8725ff36
Joaquin <quino.l85@gmail.com>
2025-01-08 13:56:04 +0100

    Tercer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
